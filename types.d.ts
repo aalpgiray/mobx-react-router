c4d158edcc1c2bf46269d3a9b9c2b03b6b93b3c0
aalpgiray <alpgirayaykut@gmail.com>
2017-03-07 22:39:21 +0200

    History interface was not up to date..

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for mobx-react-router 3.1
 // Project: https://github.com/alisd23/mobx-react-router
 
+import { History } from 'history';
+
 declare namespace MobxReactRouter {
 
   export type Action = 'POP' | 'PUSH' | 'REPLACE';
@@ -16,44 +18,6 @@ declare namespace MobxReactRouter {
     goBack(): any;
     goForward(): any;
     listen(listener: ListenerLike): UnsubscribeCallback;
-  }
-
-  export interface History {
-    listenBefore(hook: TransitionHook): UnsubscribeCallback;
-    listen(listener: LocationListener): UnsubscribeCallback;
-    transitionTo(location: Location): void;
-    push(path: string): void;
-    push(location: LocationDescriptor): void;
-    replace(path: string): void;
-    replace(location: LocationDescriptor): void;
-    go(n: number): void;
-    goBack(): void;
-    goForward(): void;
-    createKey(): string;
-    createPath(path: string): string;
-    createPath(location: LocationDescriptor): string;
-    createHref(path: string): string;
-    createHref(location: LocationDescriptor): string;
-    createLocation(path: string, action?: Action, key?: string): Location;
-    createLocation(location: LocationDescriptor): Location;
-    getCurrentLocation: () => Location;
-
-    /** @deprecated */
-    setState(path: string): void;
-    /** @deprecated */
-    setState(location: LocationDescriptor): void;
-    /** @deprecated use push() instead */
-    pushState(path: string): void;
-    /** @deprecated use push() instead */
-    pushState(location: LocationDescriptor): void;
-    /** @deprecated use replace() instead */
-    replaceState(path: string): void;
-    /** @deprecated use replace() instead */
-    replaceState(location: LocationDescriptor): void;
-    /** @deprecated */
-    registerTransitionHook(hook: TransitionHook): UnsubscribeCallback;
-    /** @deprecated */
-    unregisterTransitionHook(hook: TransitionHook): void;
   }
 
   export interface Location {
